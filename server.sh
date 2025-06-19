892b5e08bacee88d4202e4be0149a5e4f4040113
Josh Ghiloni <jghiloni@havrion.com>
2025-06-18 21:35:03 -0400

    fix k3s

--- server.sh
+++ server.sh
@@ -10,9 +10,7 @@ export newIP="192.168.65.${INDEX}/18"
 
 sudo -E yq -i '.network.ethernets.ens18.addresses[0] = env(newIP)' /etc/netplan/50-cloud-init.yaml
 
-curl -sfL https://get.k3s.io | K3S_TOKEN=$2 sh -s - server --server "https://192.168.65.1:6443" --disable=service-lb,traefik
-
-sudo chmod 644 /etc/rancher/k3s/k3s.yaml
+curl -sfL https://get.k3s.io | K3S_TOKEN=$2 sh -s - server --write-kubeconfig-mode 644 --server "https://192.168.65.1:6443" --disable service-lb --disable traefik --disable local-storage
 
 kubectl get nodes
 
